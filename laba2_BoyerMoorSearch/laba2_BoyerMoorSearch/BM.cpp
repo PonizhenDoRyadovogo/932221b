9a9843058bd6938df4c7e0b6c4cb933091edcb4a
User <User@DESKTOP-RE2P47L.local>
2023-10-04 18:56:45 +0700

    Доработка кода
    1. Добавлена функция создания таблиц TAB
    2. Убрал повтор кода(одна функция вызывает другую)

--- laba2_BoyerMoorSearch/laba2_BoyerMoorSearch/BM.cpp
+++ laba2_BoyerMoorSearch/laba2_BoyerMoorSearch/BM.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
 #include<vector>
 
-int BoyerMoorSearch(std::string text, std::string sample)
+int* MakeTable(std::string text, std::string sample)
 {
 	int l_text = text.length();
 	int l_sample = sample.length();
-	int TAB[256];
+	int* TAB = new int[256];
 	int i, j, k = 0;
 	for (i = 0; i < l_text; i++) TAB[text[i]] = l_sample;
 	for (j = 0; j < l_sample - 1; j++) TAB[sample[j]] = l_sample - 1 - j;
-
-	for (i = l_sample - 1, j = l_sample - 1; i <= l_text;i = k + TAB[text[k]])
+	return TAB;
+}
+int BoyerMoorSearch(std::string text, std::string sample,int *TAB ,int start = 0, int end = -1)
+{
+	if (end == -1)
+		end = text.length();
+	int l_sample = sample.length();
+	int i, j, k = 0;
+	for (i = start + l_sample - 1, j = l_sample - 1; i <= end;i = k + TAB[text[k]])
 	{
 		for (k = i, j = l_sample - 1; j >= 0 && text[k] == sample[j]; )
 		{
@@ -19,95 +26,39 @@ int BoyerMoorSearch(std::string text, std::string sample)
 		}
 		if (j < 0)
 			return k + 1;
-		if (k >= l_text)
+		if (k >= end)
 			return -1;
 	}
-	if (i >= l_text && j < 0)
+	if (i >= end && j < 0)
 		return k + 1;
 	else return -1;
 }
 
-std::vector<int> BoyerMoorSearchInRange(std::string text, std::string sample, int start, int end)
+std::vector<int> BoyerMoorSearchInRange(std::string text, std::string sample, int *TAB ,int start, int end)
 {
 	std::vector<int> Entrance;
-	int	entrance;
-	int l_text = end;
-	int l_sample = sample.length();
-	int TAB[256];
-	int i, j, k = 0;
-	for (i = start; i < l_text; i++) TAB[text[i]] = l_sample;
-	for (j = 0; j < l_sample - 1; j++) TAB[sample[j]] = l_sample - 1 - j;
-
-	for (i = start + l_sample - 1, j = l_sample - 1; i <= l_text;i = k + TAB[text[k]])
-	{
-		for (k = i, j = l_sample - 1; j >= 0 && text[k] == sample[j]; )
-		{
-			k--;
-			j--;
-		}
-		if (j < 0)
-		{
-			entrance = k + 1;
-			break;
-		}
-		if (k >= l_text)
-		{
-			entrance = -1;
-			break;
-		}
-	}
-	if (j < 0)
-		entrance = k + 1;
-	else entrance = -1;
-
+	int	entrance = BoyerMoorSearch(text, sample, TAB ,start, end);
 	if (entrance < 0)
 		return Entrance;
-	while (entrance > 0)
+	while (entrance >= 0)
 	{
 		Entrance.push_back(entrance);
 		start = entrance + 1;
-		int l_text = end;
-		int l_sample = sample.length();
-		int TAB[256];
-		int i, j, k = 0;
-		for (i = start; i < l_text; i++) TAB[text[i]] = l_sample;
-		for (j = 0; j < l_sample - 1; j++) TAB[sample[j]] = l_sample - 1 - j;
-
-		for (i = start + l_sample - 1, j = l_sample - 1; i <= l_text;i = k + TAB[text[k]])
-		{
-			for (k = i, j = l_sample - 1; j >= 0 && text[k] == sample[j]; )
-			{
-				k--;
-				j--;
-			}
-			if (j < 0)
-			{
-				entrance = k + 1;
-				break;
-			}
-			if (k >= l_text)
-			{
-				entrance = -1;
-				break;
-			}
-		}
-		if (j < 0)
-			entrance = k + 1;
-		else entrance = -1;
+		entrance = BoyerMoorSearch(text, sample, TAB,start, end);
 	}
 	return Entrance;
 }
 
-std::vector<int> BoyerMoorSearchAll(std::string text, std::string sample)
+std::vector<int> BoyerMoorSearchAll(std::string text, std::string sample, int *TAB)
 {
-	return BoyerMoorSearchInRange(text, sample, 0, text.length());
+	return BoyerMoorSearchInRange(text, sample, TAB,0, text.length());
 }
 int main()
 {
 	std::string text = "std::move_iterator is an iterator adaptor which behaves exactly like the underlying iterator", sample = "tor";
 	std::cout << "Text: " << text << "\nSample : " << sample << std::endl;
-
-	int first_entrance = BoyerMoorSearch(text, sample);
+	int* TAB = MakeTable(text, sample);
+	int first_entrance = BoyerMoorSearch(text, sample, TAB);
 	if (first_entrance < 0)
 		std::cout << "No entrance." << std::endl;
 	else
@@ -119,7 +70,7 @@ int main()
 	std::cout << "Please, enter ending of the range: ";
 	std::cin >> end;
 
-	std::vector<int> Entrance = BoyerMoorSearchInRange(text, sample, start, end);
+	std::vector<int> Entrance = BoyerMoorSearchInRange(text, sample, TAB ,start, end);
 	if (Entrance.size() == 0)
 		std::cout << "No entrance in range(" << start << ";" << end << ")." << std::endl;
 	else
@@ -130,7 +81,7 @@ int main()
 		std::cout << "\n";
 	}
 
-	Entrance = BoyerMoorSearchAll(text, sample);
+	Entrance = BoyerMoorSearchAll(text, sample, TAB);
 	if (Entrance.size() == 0)
 		std::cout << "No entrance in all text." << std::endl;
 	else
@@ -146,7 +97,7 @@ int main()
 	std::cout << "Please, enter ending of the range: ";
 	std::cin >> end;
 
-	Entrance = BoyerMoorSearchInRange(text, sample, start, end);
+	Entrance = BoyerMoorSearchInRange(text, sample, TAB , start, end);
 	if (Entrance.size() == 0)
 		std::cout << "No entrance in range(" << start << ";" << end << ")." << std::endl;
 	else
@@ -156,4 +107,5 @@ int main()
 			std::cout << elem << " ";
 		std::cout << "\n";
 	}
+	delete[] TAB;
 }
