a9ff52b0305adc4d17931bbaaf8445ed044c79ae
User <User@DESKTOP-RE2P47L.local>
2023-11-21 18:31:34 +0700

    Сделал функции генерации матрицы char (вводом с клавиатуры и рандомным заполнением), также функция удаления матрицы char

--- FourthLab OOP/FourthLab OOP/main.cpp
+++ FourthLab OOP/FourthLab OOP/main.cpp
@@ -2,13 +2,32 @@
 #include<stdio.h>
 #include"BoolMatrix.h"
 
-void InputCharMatrix(char** matr, int lines, int columns)
+char** CharMatrix(int lines, int columns)
 {
-	for (int i = 0; i < lines; i++)
-	{
-		for (int j = 0; j < columns; j++)
-			std::cin >> &matr[i][j];
+	char** matrix = new char* [lines];
+	for (int i = 0; i < lines; i++) {
+		matrix[i] = new char[columns + 1];
+		int j = 0;
+		for (j = 0; j < columns; j++) {
+			std::cin >> matrix[i][j];
+		}
+		matrix[i][j] = '\0';
 	}
+	return matrix;
+}
+
+char** RandCharMatrix(int lines, int columns)
+{
+	char** matrix = new char* [lines];
+	for (int i = 0; i < lines; i++) {
+		matrix[i] = new char[columns + 1];
+		int j = 0;
+		for (j = 0; j < columns; j++) {
+			matrix[i][j] = (rand() % 2) == 0? '0' : '1';
+		}
+		matrix[i][j] = '\0';
+	}
+	return matrix;
 }
 
 void OutputCharMatrix(char** matr, int lines, int columns)
@@ -22,8 +41,25 @@ void OutputCharMatrix(char** matr, int lines, int columns)
 	}
 }
 
-int main()
+void DeleteMatrix(int lines, int columns, char** matrix)
 {
+	for (int i = 0; i < lines; i++) {
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+}
+
+int main()
+{	
+	srand(time(0));
+
+	char** char_matr = RandCharMatrix(2, 8);
+	OutputCharMatrix(char_matr, 2, 8);
+	BoolMatrix matr(char_matr, 2);
+	std::cout<<"matr:\n" << matr;
+	DeleteMatrix(2, 8, char_matr);
+	return 0;
+
 	BoolMatrix matrix(2, 8, 1), matrix_, matrix3(2, 16, 0);
 	std::cout << "matrix:\n";
 	std::cout << matrix;
@@ -36,7 +72,7 @@ int main()
 	matrix = ~matrix;
 	std::cout << matrix;
 
-	return 0;
+	
 	std::cout << "weight = " << matrix.WeightMatrix()<<std::endl;
 
 	BoolMatrix copy_matrix(matrix);
@@ -71,19 +107,3 @@ int main()
 
 }
 
-//char** matr;
-	//matr = new char* [5];
-	//for (int i = 0; i < 5; i++)
-	//{
-	//	matr[i] = new char[12];
-	//}
-	//InputCharMatrix(matr, 5, 12);
-	//OutputCharMatrix(matr, 5, 12);
-
-	//BoolMatrix matrix1(matr, 5);
-	//matrix1.PrintMatrix();
-
-	////delete memory of char matrix
-	//for (int i = 0; i < 5; i++)
-	//	delete[] matr[i];
-	//delete[] matr;
