4e3ece8b6d32b14d3331a8f19fcc27e4daf55d5a
User <User@DESKTOP-RE2P47L.local>
2023-12-12 23:44:57 +0700

    Создал проект, подключил BoolMatrix и BoolMatrix

---
+++ laba7_TopologicalSort/laba7_TopologicalSort/Topological.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"../../FourthLab OOP/FourthLab OOP/BoolMatrix.h"
+#include"../../ThirdLab OOP/ThirdLab OOP/BoolVector.h"
+
+
+int main()
+{
+	BoolVector vec;
+	std::cout << vec;
+	return 0;
+}
